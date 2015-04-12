31dc2f274b041c7a7c910099a6ffb9ee0199ccd3
Daddy Cool <shirling@mail.ru>
2015-04-12 22:28:39 +0600

    add select2 and relation sklad-Ciz

--- client/templates/sklad/sklad.js
+++ client/templates/sklad/sklad.js
@@ -391,10 +391,169 @@ Template.updateSkladForm.helpers({
 					{ key: 'primechanie', label: 'Примечание', sortable: true}
 			  ]
 			};
+		},
+		CizsCount: function(){
+			var skladId = this._id;
+			var cizs = Cizs.find().fetch();
+			var cizsInSklad = _.where(cizs, {mesto: skladId});
+			return cizsInSklad.length;
+		},
+		settingsListCiz: function(){
+			var skladId = this._id;
+			var cizs = Cizs.find().fetch();
+			var cizsInSklad = _.where(cizs, {mesto: skladId});
+			return {
+				collection: cizsInSklad,
+				rowsPerPage: 10,
+				showFilter: true,
+				showColumnToggles: true,
+				class: 'table table-bordered table-hover col-sm-12',
+				fields: [
+					{ 
+						key: 'delete',
+						//headerClass: 'col-md-1',
+						label: 'Удалить',
+						hideToggle: true,
+						sortable: false,
+						hidden: function () {
+							var loggedInUser = Meteor.user();
+							if (!Roles.userIsInRole(loggedInUser, ['admin','moderator'])) {
+								return true;
+							}
+						},
+						fn: function (value){
+							return new Spacebars.SafeString('<a><i class="fa fa-times fa-lg Ciz"></i></a>');
+						}
+					},
+					{ 
+						key: 'edit',
+						//headerClass: 'col-md-1',
+						label: 'Изменить / посмотреть',
+						sortable: false,
+						fn: function (value){
+							return new Spacebars.SafeString('<a><i class="fa fa-pencil fa-lg Ciz"></i></a>');
+						}
+					},
+					{ 
+						key: 'mesto',
+						label: 'Место нахождения',
+						sortable: true,
+						fn: function(value){
+							if (Sklads.findOne({_id: value})){
+								var skladOne = Sklads.findOne({_id: value});
+								return skladOne.name;
+							};
+							if (Workers.findOne({_id: value})){
+								var workerOne = Workers.findOne({_id: value});
+								return workerOne.Familia;
+							};
+							if (ObectExplyats.findOne({_id: value})){
+								var obectExplyatOne = ObectExplyats.findOne({_id: value});
+								return obectExplyatOne.name;
+							};
+						}
+					},
+					{ 
+						key: 'mesto',
+						label: 'Номер карты',
+						sortable: true,
+						fn: function(value){
+							if (Sklads.findOne({_id: value})){
+								var skladOne = Sklads.findOne({_id: value});
+								return skladOne.nomerKarti;
+							};
+							if (Workers.findOne({_id: value})){
+								var workerOne = Workers.findOne({_id: value});
+								return workerOne.nomerKarti;
+							};
+							if (ObectExplyats.findOne({_id: value})){
+								var obectExplyatOne = ObectExplyats.findOne({_id: value});
+								return obectExplyatOne.nomerKarti;
+							};
+						}
+
+					},
+					{ key: 'number', label: 'Порядковый номер', sortable: true },
+					{ 
+						key: 'typeCiz',
+						label: 'Тип СИЗ',
+						sortable: true,
+						fn: function (value) {
+							var typeCizOne = TypeCizs.findOne({_id: value});
+							return typeCizOne.name;
+						}
+					},
+					{ key: 'nameCiz', label: 'Наименование СИЗ', sortable: true},
+					{ key: 'periodPoverki', label: 'Период поверки (мес)', sortable: true},
+					{ 
+						key: 'datePoverki',
+						label: 'Дата поверки',
+						sortable: true,
+						fn: function(value){
+							return moment(value).format('DD.MM.YYYY');
+						}
+					},
+					{ 
+						key: 'dateSledPoverki',
+						label: 'Дата следующей поверки',
+						sortable: true,
+						fn: function(value){
+							return moment(value).format('DD.MM.YYYY');
+						}
+					},
+					{
+						key: 'dateSledPoverki',
+						label: 'Поверка',
+						sortable: true,
+						fn: function(value){
+							nowDate = new Date();
+							if (nowDate > value){
+								return new Spacebars.SafeString('<span class="label label-danger">Просрочено</span>');
+							} else {
+								return new Spacebars.SafeString('<span class="label label-success">Норма</span>');
+							}
+							
+						}
+					}
+				]
+			};
 		}
 });
 
-// редактировать склад
+// редактировать СИЗ
+Template.updateSkladForm.events({
+	'click .reactive-table tr': function (event) {
+		// set the blog post we'll display details and news for
+		event.preventDefault();
+		var Ciz = this;
+		// checks if the actual clicked element has the class `delete`
+		if (event.target.className == "fa fa-pencil fa-lg Ciz") {
+			Router.go('updateCizForm', {_id: this._id});
+		}
+	}
+});
+
+// удалить СИЗ
+Template.updateSkladForm.events({
+	'click .reactive-table tr': function (event) {
+		event.preventDefault();
+		var Ciz = this;
+		// checks if the actual clicked element has the class `delete`
+		if (event.target.className == "fa fa-times fa-lg Ciz") {
+				Cizs.remove(Ciz._id, function(error){
+					if(error){
+						alertify.error("Ошибка!", error);
+						console.log("Remove Error:", error);
+					} else {
+						alertify.success("СИЗ успешно удалено!");
+						console.log("Ciz Remove!");
+					}
+				});
+		}
+	}
+});
+
+// редактировать станционное. оборудование
 Template.updateSkladForm.events({
 	'click .reactive-table tr': function (event) {
 		// set the blog post we'll display details and news for
@@ -406,7 +565,7 @@ Template.updateSkladForm.events({
 		}
 	}
 });
-// удалить склад
+// удалить станционное оборудование
 Template.updateSkladForm.events({
 	'click .reactive-table tr': function (event) {
 		event.preventDefault();
