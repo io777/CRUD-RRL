fb51a069bbd4816f6b4396e95059cdfcc015a32a
Daddy Cool <shirling@mail.ru>
2015-04-23 21:50:41 +0600

    End Izmerenie

--- client/templates/izmerenie/izmerenie.js
+++ client/templates/izmerenie/izmerenie.js
@@ -14,44 +14,64 @@ Template.Izmerenie.helpers({
 					{ key: 'name', label: 'Наименование', sortable: true},
 					{ 
 						key: 'COM_data',
-						label: 'Измерение СОМ, дата',
+						label: 'Измерение СОМ, дата (1 в год)',
 						sortable: true,
 						fn: function(value){
 							if(value){
-								return moment(value).format('DD.MM.YYYY');
+								nowDate = new Date();
+								if(moment(nowDate).year() > moment(value).year()){
+									return new Spacebars.SafeString('<span class="label label-danger">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}else{
+									return new Spacebars.SafeString('<span class="label label-success">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}
 							}
 						}
 					},
 					{ key: 'COM_protokol', label: 'Измерение СОМ, протокол', sortable: true },
 					{ 
 						key: 'Metallosvazi_data',
-						label: 'Измерение металлосвязи, дата',
+						label: 'Измерение металлосвязи, дата (1 в 5 лет)',
 						sortable: true,
 						fn: function(value){
 							if(value){
-								return moment(value).format('DD.MM.YYYY');
+								nowDate = new Date();
+								if((moment(nowDate).year() - 5) > moment(value).year()){
+									return new Spacebars.SafeString('<span class="label label-danger">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}else{
+									return new Spacebars.SafeString('<span class="label label-success">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}
 							}
 						}
 					},
 					{ key: 'Metallosvazi_protokol', label: 'Измерение металлосвязи, протокол', sortable: true },
 					{ 
 						key: 'Zazemlenia_data',
-						label: 'Измерение заземления, дата',
+						label: 'Измерение заземления, дата (1 в год)',
 						sortable: true,
 						fn: function(value){
 							if(value){
-								return moment(value).format('DD.MM.YYYY');
+								nowDate = new Date();
+								if(moment(nowDate).year() > moment(value).year()){
+									return new Spacebars.SafeString('<span class="label label-danger">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}else{
+									return new Spacebars.SafeString('<span class="label label-success">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}
 							}
 						}
 					},
 					{ key: 'Zazemlenia_protokol', label: 'Измерение заземления, протокол', sortable: true },
 					{ 
 						key: 'Petli_Faza_Null_data',
-						label: 'Измерение петли фаза-ноль, дата',
+						label: 'Измерение петли фаза-ноль, дата (1 в 5 лет)',
 						sortable: true,
 						fn: function(value){
-							if(value){
-								return moment(value).format('DD.MM.YYYY');
+						if(value){
+								nowDate = new Date();
+								if((moment(nowDate).year() - 5) > moment(value).year()){
+									return new Spacebars.SafeString('<span class="label label-danger">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}else{
+									return new Spacebars.SafeString('<span class="label label-success">'+moment(value).format('DD.MM.YYYY')+'</span>');
+								}
 							}
 						}
 					},
