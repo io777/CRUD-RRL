ccf0d7a7853784658dac18574829700703b916aa
Daddy Cool <shirling@mail.ru>
2015-05-17 23:02:40 +0500

    add dashboard chart

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,9 +1,142 @@
 Template.dashboard.onRendered(function () {
-	 $('#carousel').slick({
-      dots: true,
-      arrows: true,
-      autoplay: true,
-  	  autoplaySpeed: 2000
-    });
+	$('#carousel').slick({
+		dots: true,
+		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+	//users
+	var users = Meteor.users.find({},{sort: {createdAt: 1}}).fetch();
+	var user_date = ['user_date'];
+	var user_kol = ['user_kol'];
+	var i = 0;
+
+	_.filter(users, function(user){
+		i++;
+		user_kol.push(i);
+		user_date.push(moment(user.createdAt).format('YYYY-MM-DD'));
+	})
+
+	Session.set('user_kol', user_kol);
+	Session.set('user_date', user_date);
+
+	var user_chart = c3.generate({
+		bindto: this.find('.user_chart'),
+			data: {
+				x: 'user_date',
+				columns: [['user_date'],['user_kol']],
+				names: {
+					user_kol: 'Пользователи'
+				},
+				type: 'bar',
+				labels: true,
+				colors: {
+					user_kol: '#3EA82C'
+				},
+			},
+			grid: {
+				x: {
+					show: true
+				},
+				y: {
+					show: true
+				}
+			},
+			bar: {
+				width: {
+					ratio: 0.3 // this makes bar width 50% of length between ticks
+				}
+				// or
+				//width: 100 // this makes bar width 100px
+			},
+			axis: {
+				x: {
+					type: 'timeseries',
+					tick: {
+						format: '%Y-%m-%d'
+					}
+				}
+			}
+	});
+
+	//ams
+	var amss = AMSs.find({}, {sort: {Visota_AMS: 1}}).fetch();
+	var ams_visota = ['ams_visota'];
+	var ams_name_psevdo = ['ams_name_psevdo'];
+
+	_.filter(amss, function(ams){
+		ams_visota.push(ams.Visota_AMS);
+		ams_name_psevdo.push(ams.name_psevdo);
+	})
+
+	Session.set('ams_visota', ams_visota);
+	Session.set('ams_name_psevdo', ams_name_psevdo);
+
+	var ams_chart = c3.generate({
+		bindto: this.find('.ams_chart'),
+			data: {
+				x: 'ams_name_psevdo',
+				columns: [['ams_name_psevdo'],['ams_visota']],
+				names: {
+					ams_visota: 'АМС'
+				},
+				type: 'bar',
+				labels: true
+			},
+			grid: {
+				x: {
+					show: true
+				},
+				y: {
+					show: true
+				}
+			},
+			bar: {
+				width: {
+					ratio: 0.3 // this makes bar width 50% of length between ticks
+				}
+				// or
+				//width: 100 // this makes bar width 100px
+			},
+			axis: {
+				x: {
+					type: 'category' // this needed to load string x value
+				}
+			}
+	});
+
+	this.autorun(function (tracker) {
+		user_chart.load({columns: [
+			Session.get('user_kol'),
+			Session.get('user_date'),
+			[]
+		]});
+		ams_chart.load({columns: [
+			Session.get('ams_name_psevdo'),
+			Session.get('ams_visota'),
+			[]
+		]});
+	});
 });
 
+Template.dashboard.helpers({
+	CizsCount: function(){
+		return Cizs.find().count();
+	},
+	AMSsCount: function(){
+		return AMSs.find().count();
+	},
+	ObectExplyatsCount: function(){
+		return ObectExplyats.find().count();
+	},
+	AFYsCount: function(){
+		return AFYs.find().count();
+	},
+	AlarmsCount: function(){
+		return Alarms.find().count();
+	},
+	UsersCount: function(){
+		return Meteor.users.find().count();
+	}
+});
